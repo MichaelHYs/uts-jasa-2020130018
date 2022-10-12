8f29e5e5201cbbe4444f23f5c3d2546c6a204288
MichaelHYs <michael.hansel26@gmail.com>
2022-10-12 19:04:36 +0700

    initial commit

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,9 +8,7 @@
     <title>@yield('title', 'Fox Hotel') | Fox Hotel</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
 </head>
 
 <body class="text-center">
